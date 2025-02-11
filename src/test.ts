c1aebf779ab238f5f4d80cfecd82e1998340362e
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2025-02-11 16:07:51 +0530

    updated webpack

--- src/test.ts
+++ src/test.ts
@@ -16,6 +16,14 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-context.keys().forEach(context);
+// const context = require.context('./', true, /\.spec\.ts$/);
+// context.keys().forEach(context);
 
+console.log('require.context:', require.context);
+
+if (typeof require.context !== 'function') {
+  console.error('require.context is not defined! Check your webpack configuration.');
+} else {
+  const context = require.context('./', true, /\.spec\.ts$/);
+  context.keys().forEach(context);
+}
